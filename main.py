92399025b0af9095afe2a87128ee2e6b7751965a
Buriburizaemon <vaswaniusham2212@gmail.com>
2025-03-27 22:06:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,3 @@ TARGET_URL = "https://github.com/Akshit2807/e_waste_app/releases/latest"
 @app.get("/")
 def redirect_to_github():
     return RedirectResponse(url=TARGET_URL, status_code=302)
-
-# Required for Vercel
-from mangum import Mangum
-handler = Mangum(app)
