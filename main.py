c0d08dd702e28175166bad361327fd49ea9288dc
Buriburizaemon <vaswaniusham2212@gmail.com>
2025-04-06 21:17:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
-TARGET_URL = "https://github.com/Akshit2807/e_waste_app/releases/latest"
+TARGET_URL = "https://www.youtube.com/shorts/FwoIK3Qm6CA"
 
 @app.get("/")
 def redirect_to_github():
