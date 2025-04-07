f04c9433b80a8fa3ebdb0e437452416cc2984b1a
Buriburizaemon <vaswaniusham2212@gmail.com>
2025-04-07 20:00:48 +0530

    Update main.py
    added logging @/clicks endpoint

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
-from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse, HTMLResponse
+from collections import defaultdict
+import html
 
 app = FastAPI()
 
 TARGET_URL = "https://youtu.be/asOpxvkrx1g"
+ip_clicks = defaultdict(int)
 
 @app.get("/")
-def redirect_to_github():
+async def redirect_to_github(request: Request):
+    client_ip = request.client.host
+    ip_clicks[client_ip] += 1
     return RedirectResponse(url=TARGET_URL, status_code=302)
+
+@app.get("/clicks", response_class=HTMLResponse)
+def show_clicks():
+    table_rows = "".join(
+        f"<tr><td>{html.escape(ip)}</td><td>{count}</td></tr>"
+        for ip, count in ip_clicks.items()
+    )
+    html_content = f"""
+    <html>
+        <head><title>Click Tracker</title></head>
+        <body>
+            <table border="1">
+                <tr><th>IP </th><th>Clk Cnt</th></tr>
+                {table_rows}
+            </table>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content)
